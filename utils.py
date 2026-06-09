fe251daeb1fa58315a74ac6ec15aae90d3694b98
halamajdoub1997-prog <halamajdoub1997@gmail.com>
2026-06-09 20:11:41 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,21 @@
-def compute_score(df):
-    agg = df.groupby(["country", "indicator"])["value"].mean().reset_index()
-    agg["score"] = agg["value"] * 100
-    return agg
+import pandas as pd
+
+def compute_scores(df):
+
+    # weighted score
+    df["weighted"] = df["weight"] * df["si"]
+
+    # DOMAIN SCORE
+    domain = df.groupby(["country", "domain"]).agg(
+        score=("weighted", "sum"),
+        max_score=("weight", "sum")
+    ).reset_index()
+
+    domain["domain_score"] = (domain["score"] / domain["max_score"]) * 100
+
+    # OVERALL SCORE
+    overall = domain.groupby("country").agg(
+        total_score=("domain_score", "mean")
+    ).reset_index()
+
+    return df, domain, overall
